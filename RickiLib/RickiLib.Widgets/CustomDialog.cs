ad28ac924fbcf02635e0b17d8c60191326e89b8a
ricki9@gmail.com <ricki9@gmail.com@4db3c4fc-933f-0410-8073-c9fcf1775499>
2011-02-06 04:05:57 +0000

    * CustomDialog.cs:

--- RickiLib/RickiLib.Widgets/CustomDialog.cs
+++ RickiLib/RickiLib.Widgets/CustomDialog.cs
@@ -24,15 +24,36 @@ namespace RickiLib.Widgets
 			buttonHelp = (Gtk.Button) base.AddButton (Stock.Help, (int) ResponseType.Help);
 		}
 		
-		public virtual new ResponseType Run ()
+		public new virtual ResponseType Run ()
 		{
 			ResponseType response;
+			string message;
+			
 			do {
 				response = (ResponseType) base.Run ();
+				if (response == ResponseType.Help)
+					OnHelpRequest ();
 				
-			} while (response == ResponseType.Help);
+				if (response == ResponseType.Ok)
+						if (!OnValidate (out message)) {
+							MessageDialog msg = new MessageDialog (Globals.MainWindow,
+					                                       DialogFlags.Modal, MessageType.Error,
+					                                       ButtonsType.Ok, message);
+							msg.Title = Globals.FormatWindowTitle ("Error");
+							msg.Run ();
+							msg.Destroy ();
+							response = ResponseType.Help;
+							continue;
+						}
+			}while (response == ResponseType.Help);
 			
 			return response;
+		}
+		
+		protected virtual bool OnValidate (out string message)
+		{
+			message = string.Empty;
+			return true;
 		}
 		
 		protected virtual void OnHelpClicked ()
