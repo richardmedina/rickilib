f2cd9b9668bda6c9141edb8a95d114ecb4bcc070
ricki9@gmail.com <ricki9@gmail.com@4db3c4fc-933f-0410-8073-c9fcf1775499>
2011-02-09 23:36:48 +0000

    * Factory.cs:

--- trunk/RickiLib/RickiLib.Widgets/Factory.cs
+++ trunk/RickiLib/RickiLib.Widgets/Factory.cs
@@ -119,6 +119,49 @@ namespace RickiLib.Widgets
 			
 			return button;
 		}
+		
+		public static string SelectSingleFileDialog (FileChooserAction action, string default_filename)
+		{
+			return SelectSingleFileDialog (action, default_filename, new string [0], new string [0]);	
+		}
+		
+		public static string SelectSingleFileDialog (FileChooserAction action, string default_filename, string [] filter_name, string [] filter_pattern)
+		{
+			string title = "Seleccionar archivo";
+			string filename = string.Empty;
+			
+			FileChooserDialog dialog = new FileChooserDialog (title,
+			                                                  null,
+			                                                  action);
+			dialog.CurrentName = default_filename;
+			
+			
+			//string [] filter_name = {"Archivos de Pemex Corporativo (*.prn)", "Archivos de Pemex PEP (*.txt)", "Todos los archivos (*)"};
+			//string [] filter_pattern = {"*.prn", "*.txt", "*"};
+			
+			for (int i = 0; i < filter_name.Length; i ++) {
+				FileFilter filter = new FileFilter ();
+				filter.Name = filter_name [i];
+				filter.AddPattern (filter_pattern [i]);
+				dialog.AddFilter (filter);
+			}
+			
+			dialog.AddButton (Stock.Cancel, ResponseType.Cancel);
+			if (action == FileChooserAction.Save) {
+				dialog.DoOverwriteConfirmation = true;
+				dialog.AddButton (Stock.Save, ResponseType.Ok);
+			} else if (action == FileChooserAction.Open) {
+				dialog.AddButton (Stock.Open, ResponseType.Ok);	
+			}
+			
+			if (dialog.Run () == (int) ResponseType.Ok) {
+				filename = dialog.Filename;
+			}
+			
+			dialog.Destroy ();
+			
+			return filename;
+		}
 /*		
 * DEPRECATED *
 		public static Gdk.Pixbuf ImageToPixbuf(System.Drawing.Image image)
