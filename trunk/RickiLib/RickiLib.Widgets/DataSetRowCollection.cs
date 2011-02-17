500e3fa88f0579ae92671549d663c650ccc83632
ricki9@gmail.com <ricki9@gmail.com@4db3c4fc-933f-0410-8073-c9fcf1775499>
2011-02-17 16:54:01 +0000

    * SearchEntry.cs: * DataSetView.cs: * DataSetRowCollection.cs:

---
+++ trunk/RickiLib/RickiLib.Widgets/DataSetRowCollection.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Collections.Generic;
+
+namespace RickiLib.Widgets
+{
+
+
+	public class DataSetRowCollection : List <string []>
+	{
+
+		public DataSetRowCollection ()
+		{
+		}
+	}
+}
